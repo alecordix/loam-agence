31c3a9263905a5e2ba176e1b1b2dfbc5c96b8114
alecordix <andrea.lecordix@gmail.com>
2020-11-06 15:30:37 +0100

    Cartes TEAM faites

--- js/header.js
+++ js/header.js
@@ -1,9 +1,2 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
+let d = new Date();
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
