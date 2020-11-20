edf6a81be6b6c983a2393520631919e2f840e78d
alecordix <andrea.lecordix@gmail.com>
2020-11-20 09:19:39 +0100

    Custom scrollbar / Page equipe

--- js/demo.js
+++ js/demo.js
@@ -1,242 +1,231 @@
-'use strict';
+(function () {
 
-/**
- * Demo.
- */
-var demo = (function(window, undefined) {
-
-  /**
-   * Enum of CSS selectors.
-   */
-  var SELECTORS = {
-    pattern: '.pattern',
-    card: '.card',
-    cardImage: '.card__image',
-    cardClose: '.card__btn-close',
-  };
-
-  /**
-   * Enum of CSS classes.
-   */
-  var CLASSES = {
-    patternHidden: 'pattern--hidden',
-    polygon: 'polygon',
-    polygonHidden: 'polygon--hidden'
-  };
-
-  /**
-   * Map of svg paths and points.
-   */
-  var polygonMap = {
-    paths: null,
-    points: null
-  };
-
-  /**
-   * Container of Card instances.
-   */
-  var layout = {};
-
-  /**
-   * Initialise demo.
-   */
-  function init() {
-
-    // For options see: https://github.com/qrohlf/Trianglify
-    var pattern = Trianglify({
-      width: window.innerWidth,
-      height: window.innerHeight,
-      cell_size: 90,
-      variance: 1,
-      stroke_width: 0.6,
-      color_function : function(x, y) {
-        return '#de6551';
-      }
-    }).svg(); // Render as SVG.
-
-    _mapPolygons(pattern);
-
-    _bindCards();
-  };
-
-  /**
-   * Store path elements, map coordinates and sizes.
-   * @param {Element} pattern The SVG Element generated with Trianglify.
-   * @private
-   */
-  function _mapPolygons(pattern) {
-
-    // Append SVG to pattern container.
-    $(SELECTORS.pattern).append(pattern);
-
-    // Convert nodelist to array,
-    // Used `.childNodes` because IE doesn't support `.children` on SVG.
-    polygonMap.paths = [].slice.call(pattern.childNodes);
-
-    polygonMap.points = [];
-
-    polygonMap.paths.forEach(function(polygon) {
-
-      // Hide polygons by adding CSS classes to each svg path (used attrs because of IE).
-      $(polygon).attr('class', CLASSES.polygon + ' ' + CLASSES.polygonHidden);
-
-      var rect = polygon.getBoundingClientRect();
-
-      var point = {
-        x: rect.left + rect.width / 2,
-        y: rect.top + rect.height / 2
-      };
-
-      polygonMap.points.push(point);
-    });
-
-    // All polygons are hidden now, display the pattern container.
-    $(SELECTORS.pattern).removeClass(CLASSES.patternHidden);
-  };
-
-  /**
-   * Bind Card elements.
-   * @private
-   */
-  function _bindCards() {
-
-    var elements = $(SELECTORS.card);
-
-    $.each(elements, function(card, i) {
-
-      var instance = new Card(i, card);
-
-      layout[i] = {
-        card: instance
-      };
-
-      var cardImage = $(card).find(SELECTORS.cardImage);
-      var cardClose = $(card).find(SELECTORS.cardClose);
-
-      $(cardImage).on('click', _playSequence.bind(this, true, i));
-      $(cardClose).on('click', _playSequence.bind(this, false, i));
-    });
-  };
-
-  /**
-   * Create a sequence for the open or close animation and play.
-   * @param {boolean} isOpenClick Flag to detect when it's a click to open.
-   * @param {number} id The id of the clicked card.
-   * @param {Event} e The event object.
-   * @private
-   *
-   */
-  function _playSequence(isOpenClick, id, e) {
-
-    var card = layout[id].card;
-
-    // Prevent when card already open and user click on image.
-    if (card.isOpen && isOpenClick) return;
-
-    // Create timeline for the whole sequence.
-    var sequence = new TimelineLite({paused: true});
-
-    var tweenOtherCards = _showHideOtherCards(id);
-
-    if (!card.isOpen) {
-      // Open sequence.
-
-      sequence.add(tweenOtherCards);
-      sequence.add(card.openCard(_onCardMove), 0);
-
-    } else {
-      // Close sequence.
-
-      var closeCard = card.closeCard();
-      var position = closeCard.duration() * 0.8; // 80% of close card tween.
-
-      sequence.add(closeCard);
-      sequence.add(tweenOtherCards, position);
+   //////////////////////
+	// Utils
+  //////////////////////
+    function throttle(fn, delay, scope) {
+        // Default delay
+        delay = delay || 250;
+        var last, defer;
+        return function () {
+            var context = scope || this,
+                now = +new Date(),
+                args = arguments;
+            if (last && now < last + delay) {
+                clearTimeout(defer);
+                defer = setTimeout(function () {
+                    last = now;
+                    fn.apply(context, args);
+                }, delay);
+            } else {
+                last = now;
+                fn.apply(context, args);
+            }
+        }
     }
 
-    sequence.play();
-  };
-
-  /**
-   * Show/Hide all other cards.
-   * @param {number} id The id of the clcked card to be avoided.
-   * @private
-   */
-  function _showHideOtherCards(id) {
-
-    var TL = new TimelineLite;
-
-    var selectedCard = layout[id].card;
-
-    for (var i in layout) {
-
-      var card = layout[i].card;
-
-      // When called with `openCard`.
-      if (card.id !== id && !selectedCard.isOpen) {
-        TL.add(card.hideCard(), 0);
-      }
-
-      // When called with `closeCard`.
-      if (card.id !== id && selectedCard.isOpen) {
-        TL.add(card.showCard(), 0);
-      }
+    function extend(destination, source) {
+        for (var k in source) {
+            if (source.hasOwnProperty(k)) {
+                destination[k] = source[k];
+            }
+        }
+        return destination;
     }
 
-    return TL;
-  };
+   //////////////////////
+	// END Utils
+  //////////////////////
 
-  /**
-   * Callback to be executed on Tween update, whatever a polygon
-   * falls into a circular area defined by the card width the path's
-   * CSS class will change accordingly.
-   * @param {Object} track The card sizes and position during the floating.
-   * @private
-   */
-  function _onCardMove(track) {
+   //////////////////////
+   // Scroll Module
+   //////////////////////
 
-    var radius = track.width / 2;
+    var ScrollManager = (function () {
 
-    var center = {
-      x: track.x,
-      y: track.y
-    };
+        var defaults = {
 
-    polygonMap.points.forEach(function(point, i) {
+                steps: null,
+                navigationContainer: null,
+                links: null,
+                scrollToTopBtn: null,
 
-      if (_detectPointInCircle(point, radius, center)) {
-        $(polygonMap.paths[i]).attr('class', CLASSES.polygon);
-      } else {
-        $(polygonMap.paths[i]).attr('class', CLASSES.polygon + ' ' + CLASSES.polygonHidden);
-      }
+                navigationElementClass: '.Quick-navigation',
+                currentStepClass: 'current',
+                smoothScrollEnabled: true,
+                stepsCheckEnabled: true,
+
+                // Callbacks
+                onScroll: null,
+
+                onStepChange: function (step) {
+                    var self = this;
+                    var relativeLink = [].filter.call(options.links, function (link) {
+                        link.classList.remove(self.currentStepClass);
+                        return link.hash === '#' + step.id;
+                    });
+                    relativeLink[0].classList.add(self.currentStepClass);
+                },
+
+                // Provide a default scroll animation
+                smoothScrollAnimation: function (target) {
+                    $('html, body').animate({
+                        scrollTop: target
+                    }, 'slow');
+                }
+            },
+
+            options = {};
+
+        // Privates
+        var _animation = null,
+            currentStep = null,
+            throttledGetScrollPosition = null;
+
+        return {
+
+            scrollPosition: 0,
+
+            init: function (opts) {
+
+                options = extend(defaults, opts);
+
+                if (options.steps === null) {
+                    console.warn('Smooth scrolling require some sections or steps to scroll to :)');
+                    return false;
+                }
+
+                // Allow to customize the animation engine ( jQuery / GSAP / velocity / ... )
+                _animation = function (target) {
+                    target = typeof target === 'number' ? target : $(target).offset().top;
+                    return options.smoothScrollAnimation(target);
+                };
+
+                // Activate smooth scrolling
+                if (options.smoothScrollEnabled)  this.smoothScroll();
+
+                // Scroll to top handling
+                if (options.scrollToTopBtn) {
+                    options.scrollToTopBtn.addEventListener('click', function () {
+                        options.smoothScrollAnimation(0);
+                    });
+                }
+
+                // Throttle for performances gain
+                throttledGetScrollPosition = throttle(this.getScrollPosition).bind(this);
+                window.addEventListener('scroll', throttledGetScrollPosition);
+                window.addEventListener('resize', throttledGetScrollPosition);
+
+                this.getScrollPosition();
+            },
+
+            getScrollPosition: function () {
+                this.scrollPosition = window.pageYOffset || window.scrollY;
+                if (options.stepsCheckEnabled) this.checkActiveStep();
+                if (typeof  options.onScroll === 'function') options.onScroll(this.scrollPosition);
+            },
+
+            scrollPercentage: function () {
+                var body = document.body,
+                    html = document.documentElement,
+                    documentHeight = Math.max(body.scrollHeight, body.offsetHeight,
+                        html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+                var percentage = Math.round(this.scrollPosition / (documentHeight - window.innerHeight) * 100);
+                if (percentage < 0)  percentage = 0;
+                if (percentage > 100)  percentage = 100;
+                return percentage;
+            },
+
+            doSmoothScroll: function (e) {
+                if (e.target.nodeName === 'A') {
+                    e.preventDefault();
+                    if (location.pathname.replace(/^\//, '') === e.target.pathname.replace(/^\//, '') && location.hostname === e.target.hostname) {
+                        var targetStep = document.querySelector(e.target.hash);
+                        targetStep ? _animation(targetStep) : console.warn('Hi! You should give an animation callback function to the Scroller module! :)');
+                    }
+                }
+            },
+
+            smoothScroll: function () {
+                if (options.navigationContainer !== null) options.navigationContainer.addEventListener('click', this.doSmoothScroll);
+            },
+
+            checkActiveStep: function () {
+                var scrollPosition = this.scrollPosition;
+
+                [].forEach.call(options.steps, function (step) {
+                    var bBox = step.getBoundingClientRect(),
+                        position = step.offsetTop,
+                        height = position + bBox.height;
+
+                    if (scrollPosition >= position && scrollPosition < height && currentStep !== step) {
+                        currentStep = step;
+                        step.classList.add(self.currentStepClass);
+                        if (typeof options.onStepChange === 'function') options.onStepChange(step);
+                    }
+                    step.classList.remove(options.currentStepClass);
+                });
+            },
+
+            destroy: function () {
+                window.removeEventListener('scroll', throttledGetScrollPosition);
+                window.removeEventListener('resize', throttledGetScrollPosition);
+                options.navigationContainer.removeEventListener('click', this.doSmoothScroll);
+            }
+        }
+    })();
+     //////////////////////
+     // END scroll Module
+     //////////////////////
+
+
+    //////////////////////
+    // APP init
+    //////////////////////
+
+    var scrollToTopBtn = document.querySelector('.Scroll-to-top'),
+        steps = document.querySelectorAll('.js-scroll-step'),
+        navigationContainer = document.querySelector('.Quick-navigation'),
+        links = navigationContainer.querySelectorAll('a'),
+        progressIndicator = document.querySelector('.Scroll-progress-indicator');
+
+    ScrollManager.init({
+        steps: steps,
+        scrollToTopBtn: scrollToTopBtn,
+        navigationContainer: navigationContainer,
+        links: links,
+
+        // Customize onScroll behavior
+        onScroll: function () {
+            var percentage = ScrollManager.scrollPercentage();
+            percentage >= 90 ? scrollToTopBtn.classList.add('visible') : scrollToTopBtn.classList.remove('visible');
+
+            if (percentage >= 10) {
+                progressIndicator.innerHTML = percentage + "%";
+                progressIndicator.classList.add('visible');
+            } else {
+                progressIndicator.classList.remove('visible');
+            }
+        },
+
+		// Behavior when a step changes
+		// default : highlight links
+
+		// onStepChange: function (step) {},
+
+		// Customize the animation with jQuery, GSAP or velocity
+     // default : jQuery animate()
+     // Eg with GSAP scrollTo plugin
+
+		//smoothScrollAnimation: function (target) {
+		//		TweenLite.to(window, 2, {scrollTo:{y:target}, ease:Power2.easeOut});
+  	 //}
+
     });
-  }
 
-  /**
-   * Detect if a point is inside a circle area.
-   * @private
-   */
-  function _detectPointInCircle(point, radius, center) {
+    //////////////////////
+    // END APP init
+    //////////////////////
 
-    var xp = point.x;
-    var yp = point.y;
-
-    var xc = center.x;
-    var yc = center.y;
-
-    var d = radius * radius;
-
-    var isInside = Math.pow(xp - xc, 2) + Math.pow(yp - yc, 2) <= d;
-
-    return isInside;
-  };
-
-  // Expose methods.
-  return {
-    init: init
-  };
-
-})(window);
-
-// Kickstart Demo.
-window.onload = demo.init;
+})();
